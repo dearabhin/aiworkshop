9f1981e10f890fbfca95eb8d88e5f6cc2a622528
Abhin Krishna <dearabhin@outlook.com>
2025-03-11 17:15:58 +0530

    updated

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 load_dotenv()
-
 api_key = os.getenv("API_KEY")
 
+# Initialize client
 client = genai.Client(api_key=api_key)
 
-# Generate content
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-exp",
-    contents=["whats the date today?"]
-)
+@app.route('/home', methods=['GET'])
+def home():
+    return "Hell World"
 
-# Print the generated response
-print(response.text)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        data = request.json  # Get input from request body
+        if not data or "prompt" not in data:
+            return jsonify({"error": "prompt not found in data"}), 400
+
+        prompt = data["prompt"]
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-exp",
+            contents=prompt
+        )
+
+        return jsonify({"ai_response": response.text}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
